ad7ad74bcfb18b2207a39b7a250137935890e6c2
Porter Dalton <porter.dalton2@gmail.com>
2020-01-01 22:24:46 -0500

    Added sound effects

--- menu.py
+++ menu.py
@@ -1,8 +1,19 @@
 from tkinter import *
 
-class menu:
-    def __init__(self):
-        pass
+class Menu:
+    def __init__(self, master):
+        self.master = master
+        self.master.title("Snake")
+        self.BeginGame_Button = Button(self.master, text = "Begin Game")
+        self.BeginGame_Button.pack()
+
+        
 
 
+def main():
+    root = Tk()
+    Menu(root)
+    root.mainloop()
 
+if __name__ == "__main__":
+    main()
