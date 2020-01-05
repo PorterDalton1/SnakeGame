86956dfa5dd049825cd0ebf2f658a15605adaeba
Porter Dalton <porter.dalton2@gmail.com>
2020-01-05 01:49:46 -0500

    Working more on image, now just tryig to make it look nice

--- menu.py
+++ menu.py
@@ -1,21 +1,26 @@
 from tkinter import *
+from PIL import ImageTk, Image
 
 class MenuScreen:
     def __init__(self, master):
         self.master = master
         self.master.minsize(400,500)
         self.master.maxsize(400,500)
+        self.master.configure(bg = "black")
         #self.master.configure(bg = "black")
 
-        start_Button = Button(self.master, text = "Start Snake", command = self.startGame)
+        start_Button = Button(self.master, text = "Start Snake", bg = "white", command = self.startGame)
         options_Button = Button(self.master, text = "Options", command = self.options)
-        start_Button.pack()
-        options_Button.pack()
+        start_Button.pack(side = "left")
+        options_Button.pack(side = "right")
+        
 
-        img = PhotoImage(file = "SnakeLogo.gif")
+        img = ImageTk.PhotoImage(Image.open("SnakeLogo.gif"))
         #self.canvas = Canvas(self.master, width = img.width(), height = img.height())
-        snakeImage = Label(self.master, image = img)
-        snakeImage.pack(side = LEFT)
+        self.snakeImage = Label(self.master, image = img, bg = "black")
+        self.snakeImage.image = img
+        self.snakeImage.pack(anchor = "center")
+        
 
     def startGame(self):
         pass
@@ -30,6 +35,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
-
